d617ec7de0455b11e4fd137f24ca677c571525df
Jürgen Hannuschka <hannuschka@web.de>
2024-12-20 12:27:44 +0100

    Added StringParserFactory.

---
+++ HoI4-TranslationHelper/StringParserFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoI4_TranslationHelper
+{
+    public class StringParserFactory
+    {
+        private static StringParserFactory instance;
+        public static StringParserFactory Instance
+        {
+            get
+            {
+                if( null == instance )
+                {
+                    instance = new StringParserFactory();
+                }
+                return instance;
+            }
+        }
+
+        public StringParserFactory() { }
+
+        public IStringParser CreateKey()
+        {
+            StringParserKey stringParser = new StringParserKey();
+            stringParser.StartTag = "\"";
+            IgnoreCommentLines(stringParser);
+            return stringParser;
+        }
+
+        private void IgnoreCommentLines(StringParserBase stringParserBase)
+        {
+            stringParserBase.LineIgnores.Add("#");
+            stringParserBase.LineIgnores.Add(" #");
+            stringParserBase.LineIgnores.Add("  #");
+        }
+
+    }
+}
